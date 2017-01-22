e1d4377b7b90d0842e7d418215a6e3f9f4fc5eda
Corban Swain <CorbanSwain@understanding.local>
2017-01-22 12:23:38 -0600

    First Version of a simple app that changes an image opacity.

---
+++ WUImageScaler/WUImageScaler/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WUImageScaler
+//
+//  Created by Corban Swain on 1/22/17.
+//  Copyright © 2017 CorbanSwain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        let sliderValue = sender.value
+        image.alpha = CGFloat(sliderValue)
+        label.text = "Opacity: \(String(Int(sliderValue*100)))%"
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
